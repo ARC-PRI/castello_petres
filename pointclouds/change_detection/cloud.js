b812169e5409b09294c96ccde0869e6b44cab84a
ARC-PRI <arcangelo.priore@unibas.it>
2026-05-07 17:40:23 +0200

    Initial commit

---
+++ pointclouds/change_detection/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 49901566,
+    "boundingBox": {
+        "lx": -42.10533000183106,
+        "ly": -32.04613662719727,
+        "lz": -6.1269771728515629,
+        "ux": 34.692669998168948,
+        "uy": 44.75186337280273,
+        "uz": 70.67102282714844
+    },
+    "tightBoundingBox": {
+        "lx": -42.10533000183106,
+        "ly": -32.04613662719727,
+        "lz": -6.1269771728515629,
+        "ux": 34.692669998168948,
+        "uy": 30.396863372802735,
+        "uz": 11.890022827148437
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.6650902032852173,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
