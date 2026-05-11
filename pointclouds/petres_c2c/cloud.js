2491cbf43e6eb05fc972922dbcf261d8ff0e9be8
ARC-PRI <arcangelo.priore@unibas.it>
2026-05-11 12:37:20 +0200

    upload_c2c

---
+++ pointclouds/petres_c2c/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 28535536,
+    "boundingBox": {
+        "lx": -42.032684326171878,
+        "ly": -32.59800338745117,
+        "lz": -4.852,
+        "ux": 34.65031567382813,
+        "uy": 44.084996612548838,
+        "uz": 71.831
+    },
+    "tightBoundingBox": {
+        "lx": -42.032684326171878,
+        "ly": -32.59800338745117,
+        "lz": -4.852,
+        "ux": 34.65031567382813,
+        "uy": 29.91299661254883,
+        "uz": 11.89
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.6640942692756653,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
