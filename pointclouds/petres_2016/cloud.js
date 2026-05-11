2394cd10c17de8bcd4f1caa708725ae6207edbff
ARC-PRI <arcangelo.priore@unibas.it>
2026-05-11 14:38:22 +0200

    1

---
+++ pointclouds/petres_2016/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 37332494,
+    "boundingBox": {
+        "lx": -42.253931915283207,
+        "ly": -31.863075775146485,
+        "lz": -4.827671203613281,
+        "ux": 34.6820680847168,
+        "uy": 45.07292422485352,
+        "uz": 72.10832879638673
+    },
+    "tightBoundingBox": {
+        "lx": -42.253931915283207,
+        "ly": -31.863075775146485,
+        "lz": -4.827671203613281,
+        "ux": 34.682068084716799,
+        "uy": 30.303924224853519,
+        "uz": 12.22332879638672
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.6662852764129639,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
